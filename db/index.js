3c4b2a41448ec7ccc0f6867e2e202ae803227e7f
Mariana Danielová <danielova.mariana@gmail.com>
2019-02-04 18:55:32 +0100

    Port modified

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ const pg = require('pg');
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: true,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,    
--- index.js
+++ index.js
@@ -43,7 +43,7 @@ xAdmin.init(config, function (err, admin) {
     })
   });
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
       console.log("Server running on port 3000");
   });
 });
