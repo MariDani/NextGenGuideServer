3489719ab9620c2a133f900279b41ad9eb491b7d
Zuzana Arnoldová <zuzana.arnoldova@gmail.com>
2019-02-26 01:14:22 +0100

    Change username to user

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const client = new pg.Client({
   // connectionString: process.env.DATABASE_URL,
   ssl: true,
   host: "ec2-54-228-224-37.eu-west-1.compute.amazonaws.com",
-  username: "qqrvtrdyciuoqa",
+  user: "qqrvtrdyciuoqa",
   password: "0018497b496e77a8835f995149204e890826c0e31a7ce4c9b72a66fe590e065a",    
   database: "de2l7jhij3adpn",
   port: 5432,
