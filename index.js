2b66b5af2cc4155687f263b9b4e942959862e889
Zuzana Arnoldova <zuzana.arnoldova@merck.com>
2019-03-02 21:46:31 +0100

    Adjust db settings for Heroku

--- index.js
+++ index.js
@@ -4,9 +4,43 @@ var express = require("express");
 var xAdmin = require('express-admin');
 const db = require('./db')
 
+var prod = process.env.NODE_ENV === 'production';
+let pgConfig;
+
+if (prod) {
+   pgConfig =  {
+     "database": "de2l7jhij3adpn",
+     "host":"ec2-54-228-224-37.eu-west-1.compute.amazonaws.com",
+     "user": "qqrvtrdyciuoqa",
+     "password": "0018497b496e77a8835f995149204e890826c0e31a7ce4c9b72a66fe590e065a",
+     "schema": "public"
+    }
+} else {
+   pgConfig =  {
+      "database": "next-gen-guide-development",
+      "user": "arnoldov",
+      "password": "1",
+      "schema": "public"
+    }
+}
+
+var mainConfig =
+  {
+    "pg": pgConfig,
+    "server": {
+          "port": 3000
+      },
+      "app": {
+          "layouts": true,
+          "themes": true,
+          "languages": true,
+          "root": "/admin"
+      }
+  }
+
 var config = {
   dpath: './express-admin-config/',
-  config: require('./express-admin-config/config.json'),
+  config: mainConfig,
   settings: require('./express-admin-config/settings.json'),
   custom: require('./express-admin-config/custom.json'),
   users: require('./express-admin-config/users.json')
