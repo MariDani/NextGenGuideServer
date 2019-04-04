455a0d2dbd8ac0040841573e2b1b0c644087a31e
Mariana Danielová <danielova.mariana@gmail.com>
2019-04-04 21:49:54 +0200

    CORS to localhost (only for development)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var prod = process.env.NODE_ENV === 'production';
 let pgConfig;
 
 var corsOptions = {
-  origin: ["http://www.nextgenguide.com", "0.0.0.0:2015"],
+  origin: ["http://www.nextgenguide.com", "http://localhost:2015"],
   credentials: true
 }
 
